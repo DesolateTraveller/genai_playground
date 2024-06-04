83fac3cc71f92781f85c098dd69dd6299933a42e
Avijit Chakraborty <74238071+DesolateTraveller@users.noreply.github.com>
2024-06-04 11:22:41 +0530

    Update genai_playground.py

--- genai_playground.py
+++ genai_playground.py
@@ -20,8 +20,8 @@ st.set_page_config(page_title="GenAI | Playground",
                    initial_sidebar_state="collapsed")
 #----------------------------------------
 st.title(f""":rainbow[Generative AI | Playground | v0.1]""")
-st.caption("**:blue-background[This app allows you to chat with a PDF using Llama3 running locally with Ollama!]**")
-st.markdown('Created by | <a href="mailto:avijit.mba18@gmail.com">Avijit Chakraborty</a>', 
+#st.caption("**:blue-background[This app allows you to chat with a PDF using Llama3 running locally with Ollama!]**")
+st.markdown(':blue-background[This app allows you to chat with a PDF using Llama3 running locally with Ollama!] | Created by | <a href="mailto:avijit.mba18@gmail.com">Avijit Chakraborty</a>', 
             unsafe_allow_html=True)
 st.info('**Disclaimer : :blue[Thank you for visiting the app] | Unauthorized uses or copying of the app is strictly prohibited | Click the :blue[sidebar] to follow the instructions to start the applications.**', icon="ℹ️")
 #----------------------------------------
